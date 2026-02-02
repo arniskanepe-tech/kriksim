f2f4fe056e2f06586c8b59b4a6078d89e6a70d32
arniskanepe-tech <arnis.kanepe@gmail.com>
2026-02-02 19:32:16 +0200

    Update designs.js

--- assets/designs.js
+++ assets/designs.js
@@ -6,5 +6,6 @@ window.KR_DESIGNS = [
   { id: 2, file: "index2.html", name: "Side-nav plakāts", note: "Kreisā fiksētā izvēlne + manifesto." },
   { id: 3, file: "index3.html", name: "Balanss apļos", note: "Centra portrets + harmonijas apļi." },
   { id: 4, file: "index4.html", name: "Stāsts / Timeline", note: "Ceļš no pieredzes līdz izaugsmei." },
-  { id: 5, file: "index5.html", name: "Magazine scroller", note: "Horizontāla pakalpojumu lente + FAQ." }
+  { id: 5, file: "index5.html", name: "Magazine scroller", note: "Horizontāla pakalpojumu lente + FAQ." },
+  { id: 6, file: "index6.html", name: "Izaugsmes ceļš", note: "Journey map + stāsta sekcijas." }
 ];
